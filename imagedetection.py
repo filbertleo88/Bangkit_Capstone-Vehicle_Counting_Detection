aa5673820b1282f106e692e692b1bc9c0f774212
filbertleo88 <filbertleo88@gmail.com>
2023-06-13 14:23:42 +0700

    clean_code

--- imagedetection.py
+++
@@ -1,6 +0,0 @@
-from ultralytics import YOLO
-import cv2
-
-model = YOLO("models/model_custom.pt")
-results = model("image/sample4.jpg", show=True)
-cv2.waitKey(0)
--- main.py
+++ main.py
@@ -7,11 +7,14 @@ from sort import *
 import csv
 from firebase import firebase
 
-cap = cv2.VideoCapture("videos/test2.mp4")  # For Video
+cap = cv2.VideoCapture("videos/test1.mp4")  # For Video
 # cap = cv2.VideoCapture(1) # For Webcam
 
 frame_width = int(cap.get(3))
 frame_height = int(cap.get(4))
+
+#Output Video
+out=cv2.VideoWriter('output.avi', cv2.VideoWriter_fourcc('M', 'J', 'P','G'), 10, (frame_width, frame_height))
 
 # Custom model
 model = YOLO("models/model_custom.pt")
@@ -24,21 +27,21 @@ classNames=["big bus", "big truck", "bus-l-", "bus-s-", "car", "mid truck", "sma
 tracker = Sort(max_age=20, min_hits=3, iou_threshold=0.3)
 
 # Line
-limits = [30, 450, 568, 450]
-limits1 = [680, 450, 1300, 450]
+line_up = [30, 450, 568, 450]
+line_down = [680, 450, 1300, 450]
 
 # Counter
-totalCount = []
-totalCount1 = []
-
+total_count_in = []
+total_count_out = []
 capacity = input("Enter rest area capacity: ")
-firebase =firebase.FirebaseApplication('https://interstate-now-default-rtdb.asia-southeast1.firebasedatabase.app/',None)
+
+firebase = firebase.FirebaseApplication('https://interstate-now-default-rtdb.asia-southeast1.firebasedatabase.app/',None)
 
 while True:
-    success, img = cap.read()
-    img = cv2.resize(img, (1280, 720))
+    success, frame = cap.read()
+    frame = cv2.resize(frame, (1280, 720))
 
-    results = model(img, stream=True)
+    results = model(frame, stream=True)
 
     detections = np.empty((0, 5))
 
@@ -72,16 +75,16 @@ while True:
                 color = (255, 255, 255)
 
             if currentClass in classNames and conf > 0.4:
-                cvzone.cornerRect(img, (x1, y1, w, h), l=7, rt=5, colorR=color)
-                cvzone.putTextRect(img, f"{label} ", (max(0, x1), max(35, y1)), scale=1, thickness=2,
+                cvzone.cornerRect(frame, (x1, y1, w, h), l=7, rt=5, colorR=color)
+                cvzone.putTextRect(frame, f"{label} ", (max(0, x1), max(35, y1)), scale=1, thickness=2,
                                    offset=5, colorR=color)
                 currentArray = np.array([x1, y1, x2, y2, conf])
                 detections = np.vstack((detections, currentArray))
 
     resultsTracker = tracker.update(detections)
 
-    cv2.line(img, (limits[0], limits[1]), (limits[2], limits[3]), (0, 0, 255), 5)
-    cv2.line(img, (limits1[0], limits1[1]), (limits1[2], limits1[3]), (0, 0, 255), 5)
+    cv2.line(frame, (line_up[0], line_up[1]), (line_up[2], line_up[3]), (0, 0, 255), 5)
+    cv2.line(frame, (line_down[0], line_down[1]), (line_down[2], line_down[3]), (0, 0, 255), 5)
 
 
     for results in resultsTracker:
@@ -92,30 +95,30 @@ while True:
 
         # Center Point
         cx, cy = x1 + w // 2, y1 + h // 2
-        cv2.circle(img, (cx, cy), 5, (255, 0, 255), cv2.FILLED)
+        cv2.circle(frame, (cx, cy), 5, (255, 0, 255), cv2.FILLED)
 
         # Counting
-        if limits[0] < cx < limits[2] and limits[1] - 20 < cy < limits[3] + 20:
-            if totalCount.count(id) == 0:
-                totalCount.append(id)
+        if line_up[0] < cx < line_up[2] and line_up[1] - 20 < cy < line_up[3] + 20:
+            if total_count_in.count(id) == 0:
+                total_count_in.append(id)
                 # Change line color
-                cv2.line(img, (limits[0], limits[1]), (limits[2], limits[3]), (0, 255, 0), 5)
+                cv2.line(frame, (line_up[0], line_up[1]), (line_up[2], line_up[3]), (0, 255, 0), 5)
 
-        if limits1[0] < cx < limits1[2] and limits1[1] - 20 < cy < limits1[3] + 20:
-            if totalCount1.count(id) == 0:
-                totalCount1.append(id)
+        if line_down[0] < cx < line_down[2] and line_down[1] - 20 < cy < line_down[3] + 20:
+            if total_count_out.count(id) == 0:
+                total_count_out.append(id)
                 # Change line color
-                cv2.line(img, (limits1[0], limits1[1]), (limits1[2], limits1[3]), (0, 255, 0), 5)
+                cv2.line(frame, (line_down[0], line_down[1]), (line_down[2], line_down[3]), (0, 255, 0), 5)
 
-    total_vehicle_in = str(len(totalCount))
-    total_vehicle_out = str(len(totalCount1))
+    total_vehicle_in = str(len(total_count_in))
+    total_vehicle_out = str(len(total_count_out))
     total_capacity = int(capacity) - int(total_vehicle_in) + int(total_vehicle_out)
 
-    cv2.putText(img, f"vehicle in: {total_vehicle_in}", (100, 100), cv2.FONT_HERSHEY_PLAIN, 2, (50, 50, 255), 3)
-    cv2.putText(img, f"vehicle out: {total_vehicle_out}",(800,100),cv2.FONT_HERSHEY_PLAIN,2,(50,50,255),3)
+    cv2.putText(frame, f"Vehicle in: {total_vehicle_in}", (100, 100), cv2.FONT_HERSHEY_PLAIN, 2, (50, 50, 255), 3)
+    cv2.putText(frame, f"Vehicle out: {total_vehicle_out}",(800,100),cv2.FONT_HERSHEY_PLAIN,2,(50,50,255),3)
 
-    print("In:"+total_vehicle_in)
-    print("Out"+total_vehicle_out)
+    print("In: "+total_vehicle_in)
+    print("Out: "+total_vehicle_out)
 
     # CSV
     output_file = 'output.csv'
@@ -125,10 +128,16 @@ while True:
         writer.writerow([total_vehicle_in,total_vehicle_out,total_capacity])
     csvfile.close()
 
-    cv2.imshow("Image", img)
-    cv2.waitKey(1)
-
-    # # Firebase
+    # Firebase
     result=firebase.put('/count','capacity',int(total_capacity))
     result=firebase.put('/count','in',int(total_vehicle_in))
     result=firebase.put('/count','out',int(total_vehicle_out))
+
+    cv2.imshow("Video", frame)
+    out.write(frame)
+    if cv2.waitKey(1)&0xff==ord('q'):
+            break
+
+out.release()
+cap.release()
+cv2.destroyAllWindows()
